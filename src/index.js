89483371aa0aa7fdafac29c64b0cc0495ec16e22
Saad Aulakh <saadaulakh04@gmail.com>
2022-09-05 21:52:28 -0400

    Added JS code to generate HTML items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import * as module from './initial-page-load.js'
 console.log("This is a console.log statement from /src/index.js lol")
+module.initialPageLoad();
---
+++ src/initial-page-load.js
@@ -0,0 +1,28 @@
+export function initialPageLoad() {
+    const contentDiv = document.querySelector("#content"); 
+    const heading = document.createElement("h1");
+    heading.textContent = "Welcome to Saad's Restaurant!";
+    heading.classList.add("landing-title");
+    contentDiv.appendChild(heading);
+
+    const topImage = document.createElement("img");
+    topImage.classList.add("top-image");
+    topImage.src = " ../src/assets/restaurant-image.jpeg";
+    topImage.alt = "Image of Restaurant";
+    contentDiv.appendChild(topImage);
+
+    const paral = document.createElement("p");
+    paral.classList.add("landing-page-copy");
+    paral.textContent = "This is a very upscale restaurant that is well known around the world";
+    contentDiv.appendChild(paral);
+
+    const para2 = document.createElement("p");
+    para2.classList.add("landing-page-copy");
+    para2.textContent = "Please wear business casual attire in this restaurant!";
+    contentDiv.appendChild(para2);
+    
+    const para3 = document.createElement("p");
+    para3.classList.add("landing-page-copy");
+    para3.textContent = "Thanks!";
+    contentDiv.appendChild(para3);
+}
